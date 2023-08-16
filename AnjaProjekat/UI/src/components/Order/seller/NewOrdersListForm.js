37d2a76e4b2b8dde46651ae8d28d40d232f72558
anjapuskas <anjapuskas@hotmail.com>
2023-08-17 00:32:51 +0200

    dodavanje countdown timera

--- AnjaProjekat/UI/src/components/Order/seller/NewOrdersListForm.js
+++ AnjaProjekat/UI/src/components/Order/seller/NewOrdersListForm.js
@@ -6,6 +6,7 @@ import { Button, Container, CssBaseline, Grid, Paper, Table, TableBody, TableCel
 import Navigation from 'components/Navigation/Navigation';
 import { cancelOrder, getAllOrdersAction, getNewOrdersAction } from 'slices/orderSlice';
 import { useNavigate } from 'react-router';
+import CountdownTimer from 'components/Shared/CountdownTimer';
 
 const NewOrdersListForm = () => {
   const dispatch = useDispatch();
@@ -37,9 +38,9 @@ const NewOrdersListForm = () => {
           <TableCell>User Id</TableCell>
             <TableCell>Address</TableCell>
             <TableCell>Created Date</TableCell>
-            <TableCell>Delivery Date</TableCell>
             <TableCell>Price</TableCell>
             <TableCell>Actions</TableCell>
+            <TableCell>Delivery Time</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -48,13 +49,17 @@ const NewOrdersListForm = () => {
               <TableCell>{order.userId}</TableCell>
               <TableCell>{order.address}</TableCell>
               <TableCell>{order.created}</TableCell>
-              <TableCell>{order.deliveryTime}</TableCell>
               <TableCell>{order.price}</TableCell>
               <TableCell>
                 <Button variant="outlined" color="secondary" onClick={() => handleDetails(order.id)}>
                   Details
                 </Button>                
               </TableCell>
+              {order.orderStatus === 'ORDERED' ? (
+              <CountdownTimer deliveryTime={order.deliveryTime} />
+              ) : (
+                <TableCell>{order.deliveryTime}</TableCell>
+                )}
             </TableRow>
           ))}
         </TableBody>
